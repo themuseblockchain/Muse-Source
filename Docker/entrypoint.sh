72c6612727dfe7db2cebf641811ceea046e24bbd
educatedwarrior <educatedwarrior@fusemail.com>
2017-07-22 18:28:00 -0400

    Removed git -j switch

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -13,27 +13,39 @@ fi
 if [ "${NODE_TYPE}" = "test" ]; then
 	if [ -f "${MUSE_DATADIR}/genesis-test.json" ]
 	  then
-	    echo
+	    echo "Starting muse daemon in TEST"
+	  	exec mused -s ${TEST_SEED} \
+		--rpc-endpoint=0.0.0.0:8090 \
+		--genesis-json ${MUSE_DATADIR}/genesis-test.json \
+		-d ${MUSE_DATADIR}/
 	  else
+	  	echo "Starting muse daemon in TEST.  Replaying blockchain"
 	    cp /genesis-test.json ${MUSE_DATADIR}
+	    exec mused -s ${TEST_SEED} \
+		--replay-blockchain --rpc-endpoint=0.0.0.0:8090 \
+		--genesis-json ${MUSE_DATADIR}/genesis-test.json \
+		-d ${MUSE_DATADIR}/
 	fi
-	exec mused -s ${TEST_SEED} \
-	--replay-blockchain --rpc-endpoint=0.0.0.0:8090 \
-	--genesis-json ${MUSE_DATADIR}/genesis-test.json \
-	-d ${MUSE_DATADIR}/
+
 fi
 
 if [ "${NODE_TYPE}" = "prod" ]; then
-	if [ -f "${MUSE_WORKDIR}/genesis.json" ]
+	if [ -f "${MUSE_DATADIR}/genesis.json" ]
 	  then
-	    echo
+	    echo "Starting muse daemon in PROD"
+	    exec mused -s ${PROD_SEED} \
+		--rpc-endpoint=0.0.0.0:8090 \
+		--genesis-json ${MUSE_DATADIR}/genesis.json \
+		-d ${MUSE_DATADIR}/
 	  else
+	  	echo "Starting muse daemon in PROD.  Replaying blockchain"
 	    cp /genesis.json ${MUSE_DATADIR}
+		exec mused -s ${PROD_SEED} \
+		--replay-blockchain --rpc-endpoint=0.0.0.0:8090 \
+		--genesis-json ${MUSE_DATADIR}/genesis.json \
+		-d ${MUSE_DATADIR}/
 	fi
-	exec mused -s ${PROD_SEED} \
-	--replay-blockchain --rpc-endpoint=0.0.0.0:8090 \
-	--genesis-json ${MUSE_DATADIR}/genesis.json \
-	-d ${MUSE_DATADIR}/
+
 fi
 
 
