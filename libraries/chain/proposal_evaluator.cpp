20cab55f420681d9a0878087026a7283ae53619d
Peter Conrad <conrad@quisquis.de>
2018-06-21 18:35:11 +0200

    Another HF protection cleaned up, verified using snapshots @ 8820000

--- libraries/chain/proposal_evaluator.cpp
+++ libraries/chain/proposal_evaluator.cpp
@@ -118,28 +118,25 @@ void proposal_create_evaluator::do_apply(const proposal_create_operation& o)
       proposal.can_veto.insert( proposal.required_owner_approvals.begin(), proposal.required_owner_approvals.end() );
       proposal.can_veto.insert( proposal.required_basic_approvals.begin(), proposal.required_basic_approvals.end() );
 
-      if( d.has_hardfork( MUSE_HARDFORK_0_3 ) ) // TODO: attempt to remove after HF 3
-      {
-         // Active or owner authorities also cover basic authority
-         for( const string& a : required_active )
-            proposal.required_basic_approvals.erase( a );
-         for( const string& o : proposal.required_owner_approvals )
-            proposal.required_basic_approvals.erase( o );
+      // Active or owner authorities also cover basic authority
+      for( const string& a : required_active )
+         proposal.required_basic_approvals.erase( a );
+      for( const string& o : proposal.required_owner_approvals )
+         proposal.required_basic_approvals.erase( o );
 
-         FC_ASSERT( proposal.required_basic_approvals.size() == 0
-                    || ( required_active.size() == 0
-                         && proposal.required_owner_approvals.size() == 0
-                         && proposal.required_master_content_approvals.size() == 0
-                         && proposal.required_comp_content_approvals.size() == 0
-                         && other.size() == 0 ),
-                    "Cannot combine operations with basic approval and others!" );
+      FC_ASSERT( proposal.required_basic_approvals.size() == 0
+                 || ( required_active.size() == 0
+                      && proposal.required_owner_approvals.size() == 0
+                      && proposal.required_master_content_approvals.size() == 0
+                      && proposal.required_comp_content_approvals.size() == 0
+                      && other.size() == 0 ),
+                 "Cannot combine operations with basic approval and others!" );
 
-         authority_collector collector( d, required_active );
-         for( const string& url : proposal.required_master_content_approvals )
-            collector.collect( d.get_content(url).manage_master );
-         for( const string& url : proposal.required_comp_content_approvals )
-            collector.collect( d.get_content(url).manage_comp );
-      }
+      authority_collector collector( d, required_active );
+      for( const string& url : proposal.required_master_content_approvals )
+         collector.collect( d.get_content(url).manage_master );
+      for( const string& url : proposal.required_comp_content_approvals )
+         collector.collect( d.get_content(url).manage_comp );
 
       //All accounts which must provide both owner and active authority should be omitted from the active authority set
       //owner authority approval implies active authority approval.
